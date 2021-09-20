726adfebe7bc2e0e69e56ebd6d23d7dce1cfef5e
tuandung411 <tuandungpham2000@gmail.com>
2021-09-20 21:27:52 +0700

    ss

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+//check
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
-const authRouter = require('./routes/auth')
-const postRouter = require('./routes/post')
-const bookRouter = require('./routes/book')
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+const bookRouter = require("./routes/book");
 
-const connectDB = async() => {
-  try{
-    await mongoose.connect(
-      `mongodb://localhost:27017/`,
-      {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-      }
-    )
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb://localhost:27017/`, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
 
-    console.log('Conneted server')
-  }catch(error){
-    console.log(error.message),
-    process.exit(1)
-  }
-}
+        console.log("Conneted server");
+    } catch (error) {
+        console.log(error.message), process.exit(1);
+    }
+};
 
-connectDB()
+connectDB();
 
-const app = express()
-app.use(express.json())
-app.use('/api/auth', authRouter)
-app.use('/api/posts', postRouter)
-app.use('/api/book', bookRouter)
+const app = express();
+app.use(express.json());
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/book", bookRouter);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-console.log(`Example app listening at http://localhost:${PORT}`)
-})                    
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
